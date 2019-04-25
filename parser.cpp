8348ee59811e51c187f1f058c51ef447ec03cc6c
Hawkx78 <33515788+Hawkx78@users.noreply.github.com>
2019-04-25 19:08:30 +0200

    Added def of parser and functions

--- parser.cpp
+++ parser.cpp
@@ -4,3 +4,57 @@ Parser::Parser()
 {
 
 }
+
+void  ParseFile(string* path)
+{
+
+}
+
+string  GetDataToBeSaved(unsigned  int  startGen , unsigned  int  endGen)
+{
+
+}
+
+string  GetDataToBeSaved()
+{
+
+}
+
+void  ParseAndAddRules(string* index)
+{
+
+}
+
+void  ParseAndAddStates(string* index)
+{
+
+}
+
+void  ParseAndAddType(string* index)
+{
+
+}
+void  ParseAndAddSize(string* index)
+{
+
+}
+
+void  ParseHistory(string* index)
+{
+
+}
+
+string  AutomataToString()
+{
+
+}
+
+string  HistoryToString()
+{
+
+}
+
+string  RulesToString()
+{
+
+}
--- parser.h
+++ parser.h
@@ -1,11 +1,30 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "automata.h"
 
 class Parser
 {
 public:
     Parser();
+    void  ParseFile(const  string* path);
+    string  GetDataToBeSaved(unsigned  int  startGen , unsigned  int  endGen);
+    string  GetDataToBeSaved();
+    void  ParseAndAddRules(string* index);//Fonction qui interprète les regles de l’automate.
+    void  ParseAndAddStates(string* index);//Fonction qui interprète les etats de l’automate.
+    void  ParseAndAddType(string* index);//Fonction qui interprète la type de l’automate.
+    void  ParseAndAddSize(string* index);//Fonction qui interprète la taille de l’automate
+
+private:
+    Automata* automata;
+    string  dataToParse;
+
+    void  ParseHistory(string* index);//Fonction parsant l’historique, chargée de récupérer les matrices de l’historiques’il existe,
+                                      //elle peut lever l’exception (HISTORY) si par exemple il manque des générations.
+                                      //Elle ajoute la génération à l’historique
+    string  AutomataToString() ;//Fonctions qui calculent la représentation en chaîne de caractères de l’automate.
+    string  HistoryToString();
+    string  RulesToString();
 };
 
 #endif // PARSER_H
