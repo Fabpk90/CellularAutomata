1b2b8cc40d8bd091f64a6a3cd76543ecf6566595
Jilink <jilinkk@hotmail.fr>
2019-05-02 11:18:15 +0200

    compute position

--- ruledeterministic.cpp
+++ ruledeterministic.cpp
@@ -9,17 +9,6 @@ RuleDeterministic::RuleDeterministic(bool isComputePosition, State* toChangeInto
 
 void RuleDeterministic::Apply(int x, int y){
 
-    /* pour computeposition :
-    bool ruleTrue = true; // utilisé pour tester si tous les params sont vrais
-    for( RuleParameters & Rparams : this->parameters) { // on parcours chaque paramètres de la règle
-        if (GetCellState(x+Rparams.x,y+Rparams.y).name == Rparams.toCheckAgainst.name ){ // test entre la position relative entre la case central et celles de la règle pour voir si l'état est le même
-            ruleTrue= ruleTrue && true;
-        }
-        else {
-            ruleTrue=false;
-        }
-    }
-    */
 
     if(this->isComputePosition){ // on verifie si on doit computeposition ou computecount
         // dans computePositon et l'autre compute le seul paramètre devrait être le vecteur de rulesParameters
--- simulator.cpp
+++ simulator.cpp
@@ -1,9 +1,18 @@
 #include "simulator.h"
-
+Automata automata; // TEMPORAIRE POUR EVITER ERRRORS
 bool ComputePosition(std::vector<std::pair<int , int >> positions ,std::vector<State*> testState , int  targetX , int  targetY){
-
-    return true;
-};
+    bool ruleTrue = true; // utilisé pour tester si tous les params sont vrais
+    for(int i=0; i<positions.size(); i++)
+    {
+        if (automata.GetCellState(targetX+positions[i].first,targetY+positions[i].second).name == testState[i]->name ){ // test entre la position relative entre la case central et celles de la règle pour voir si l'état est le même
+            ruleTrue= ruleTrue && true;
+        }
+        else {
+            ruleTrue=false;
+        }
+    }
+    return ruleTrue;
+}
 
 bool ComputeCount(int amount, State* &toCheckAgainst, int posX, int posY){
 
