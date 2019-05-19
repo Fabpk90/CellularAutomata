58e376558acc19f1c3ec174abd47d6ac16401192
lucas <lucas.lebret@ens.uvsq.fr>
2019-05-19 12:02:55 +0200

    preemptive bug fixing

--- simulator.cpp
+++ simulator.cpp
@@ -9,15 +9,27 @@ void Simulate(Automata& automata)
     Generation newest;
     newest.generationID = automata.GetCurrentGen().generationID + 1;
     automata.AddGeneration(newest);
+    State oldState;
+
     for(unsigned int i = 0; i < automata.GetSizeX(); i++)
     {
         for(unsigned int j = 0; j < automata.GetSizeY(); j++)
         {
+            //Init la cellule de la nouvelle gen à son ancienne valeur
+            oldState = automata.GetCellState(i,j);
+            automata.NextGen();
+            automata.SetCell(i,j, oldState);
+            automata.PreviousGen();
+
             for(unsigned int k = 0; k < automata.GetRules().size(); k++)
                 if(automata.GetRules()[k]->GetStartingState().color == automata.GetCellState(i,j).color) //Applique la règle seulement si l'état de départ correspond à l'état de la case testé
+                {
                     automata.GetRules()[k]->Apply(i,j);
+                }
         }
     }
+
+    automata.NextGen();
 }
 
 bool ComputePosition(std::vector<std::pair<int , int >> positions ,std::vector<State*> testState , int  targetX , int  targetY){
